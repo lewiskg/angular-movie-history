5a0619f2b3dc8d38427878dd3ae8d9a0c6bf3d64
lewiskg <kenneth.g.lewis2@gmail.com>
2017-11-13 19:40:00 -0600

    angular intialized

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,3 +1,5 @@
 "use strict";
 
-console.log('hello');
+/* globals app: true */ // Ignore that app is defined in Gruntfile.js.  "const app" is necessary for browser to define variable
+
+const app = angular.module("MovieApp", ["ngAnimate", "ngRoute", "ngSanitize"]);
