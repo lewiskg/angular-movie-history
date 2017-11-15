aebda4cf38db39d5f48d52655d4dc3dea8f165c0
lewiskg <kenneth.g.lewis2@gmail.com>
2017-11-14 19:10:03 -0600

    working on SearchCtrl.js

---
+++ javascripts/services/tmdbService.js
@@ -0,0 +1,8 @@
+"use strict";
+
+app.service("tmdbService", function($http, TMDB_CONFIG) { // $http = $.ajax ; TMDB_CONFIG is a constant in AppConstants.js file
+	const searchMovies = (query) => {
+		return $http.get(`https://api.themoviedb.org/3/search/movie?api_key=${TMDB_CONFIG}&language=en-US&page=1&include_adult=false&query=${query}`);
+	};
+	return { searchMovies };
+});
