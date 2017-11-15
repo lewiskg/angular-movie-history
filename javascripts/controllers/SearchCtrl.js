e1aa181a7614e2869c5987c13528ef716692087d
lewiskg <kenneth.g.lewis2@gmail.com>
2017-11-14 20:33:54 -0600

    movie posters visible

--- javascripts/controllers/SearchCtrl.js
+++ javascripts/controllers/SearchCtrl.js
@@ -1,12 +1,14 @@
 'use strict';
 
 app.controller("SearchCtrl", function($scope, tmdbService){
+	$scope.movies = [];
 
 	$scope.enterPush = (event) => {
 		if(event.keyCode === 13){
 			console.log("event", event.target.value);
 			tmdbService.searchMovies(event.target.value).then((results) => {
-			console.log("movies?", results.data.results);
+				$scope.movies = results.data.results;
+				console.log("movies?", results.data.results);
 		}).catch((err) => {
 			console.log("error in searchMovies", err);
 		});
